c6db08b80ccfff320e55f11bdf42463139d9fbd9
RodrigoScola <rodrigo.sgarabotto.scola@gmail.com>
2023-09-06 12:33:57 -0300

    reyna, killjoy, breach abilities added

--- src/views/Habilities.ts
+++ src/views/Habilities.ts
@@ -83,6 +83,34 @@ export class HabilityFactory {
           HabilityFactory.getHability(AbilitiesIds.OWL_DRONE),
           HabilityFactory.getHability(AbilitiesIds.HUNTERS_FURY),
         ];
+      case CharacterName.VIPER:
+        return [
+          HabilityFactory.getHability(AbilitiesIds.POISON_CLOUD),
+          HabilityFactory.getHability(AbilitiesIds.TOXIC_SCREEN),
+          HabilityFactory.getHability(AbilitiesIds.SNAKE_BITE),
+          HabilityFactory.getHability(AbilitiesIds.VIPERS_PIT),
+        ];
+      case CharacterName.CYPHER:
+        return [
+          HabilityFactory.getHability(AbilitiesIds.CYBER_CAGE),
+          HabilityFactory.getHability(AbilitiesIds.SPY_CAM),
+          HabilityFactory.getHability(AbilitiesIds.TRAPWIRE),
+          HabilityFactory.getHability(AbilitiesIds.NEUTRAL_THEFT),
+        ];
+      case CharacterName.REYNA:
+        return [
+          HabilityFactory.getHability(AbilitiesIds.DEVOUR),
+          HabilityFactory.getHability(AbilitiesIds.DISMISS),
+          HabilityFactory.getHability(AbilitiesIds.LEER),
+          HabilityFactory.getHability(AbilitiesIds.EMPRESS),
+        ];
+      case CharacterName.KILLJOY:
+        return [
+          HabilityFactory.getHability(AbilitiesIds.ALARMBOT),
+          HabilityFactory.getHability(AbilitiesIds.TURRET),
+          HabilityFactory.getHability(AbilitiesIds.NANOSWARM),
+          HabilityFactory.getHability(AbilitiesIds.LOCKDOWN),
+        ];
       default:
         return [
           HabilityFactory.getHability(AbilitiesIds.SLOW_ORB),
@@ -208,28 +236,130 @@ export class HabilityFactory {
           description:
             "EQUIP a chemical launcher. FIRE to launch a canister that shatters upon hitting the floor, creating a lingering chemical zone that damages and slows enemies.",
         });
+      case AbilitiesIds.VIPERS_PIT:
+        return new CharacterUltimateAbility({
+          name: "Vipers Pit",
+          description:
+            "EQUIP a chemical sprayer. FIRE to spray a chemical cloud in all directions around Viper, creating a large cloud that reduces the vision range and maximum health of players inside of it.",
+        });
+      case AbilitiesIds.CYBER_CAGE:
+        return new BaseHability({
+          name: "Cyber Cage",
+          description:
+            "INSTANTLY toss the cyber cage in front of Cypher. Activate to create a zone that blocks vision and slows enemies who pass through it.",
+        });
+      case AbilitiesIds.SPY_CAM:
+        return new BaseHability({
+          name: "Spy Cam",
+          description:
+            "EQUIP a spycam. FIRE to place the spycam at the targeted location. RE-USE this ability to take control of the camera's view. While in control of the camera, FIRE to shoot a marking dart. This dart will Reveal the location of any player struck by the dart. This ability can be picked up to be REDEPLOYED.",
+        });
+      case AbilitiesIds.TRAPWIRE:
+        return new BaseHability({
+          name: "Trapwire",
+          description:
+            "EQUIP a trapwire. FIRE to place a destructible and covert tripwire at the targeted location creating a line that spans between the placed location and the wall opposite. Enemy players who cross a tripwire will be tethered, revealed, and dazed after a short period if they do not destroy the device in time. This ability can be picked up to be REDEPLOYED.",
+        });
+      case AbilitiesIds.NEUTRAL_THEFT:
+        return new CharacterUltimateAbility({
+          name: "Neural Theft",
+          description:
+            "INSTANTLY use on a dead enemy player in your crosshairs to reveal the location of all living enemy players.",
+        });
+      case AbilitiesIds.DEVOUR:
+        return new BaseHability({
+          name: "Devour",
+          description:
+            "Enemies killed by Reyna leave behind Soul Orbs that last 3 seconds. INSTANTLY consume a nearby soul orb, rapidly healing for a short duration. Health gained through this skill exceeding 100 will decay over time. If EMPRESS is active, this skill will automatically cast and not consume the orb.",
+        });
+      case AbilitiesIds.DISMISS:
+        return new BaseHability({
+          name: "Dismiss",
+          description:
+            "INSTANTLY consume a nearby soul orb, becoming intangible for a short duration. If EMPRESS is active, also become invisible",
+        });
+      case AbilitiesIds.LEER:
+        return new BaseHability({
+          name: "Leer",
+          description:
+            "EQUIP an ethereal destructible eye. ACTIVATE to cast the eye a short distance forward. The eye will Nearsight all enemies who look at it.",
+        });
+      case AbilitiesIds.EMPRESS:
+        return new CharacterUltimateAbility({
+          name: "Empress",
+          description:
+            "INSTANTLY enter a frenzy, increasing firing speed, equip and reload speed dramatically. Scoring a kill renews the duration.",
+        });
+
+      case AbilitiesIds.ALARMBOT:
+        return new BaseHability({
+          name: "Alarmbot",
+          description:
+            "EQUIP a covert Alarmbot. FIRE to deploy a bot that hunts down enemies that get in range. After reaching its target, the bot explodes, applying Vulernable. HOLD EQUIP to recall a deployed bot.",
+        });
+      case AbilitiesIds.TURRET:
+        return new BaseHability({
+          name: "Turret",
+          description:
+            "EQUIP a Turret. FIRE to deploy a turret that fires at enemies in a 180 degree cone. HOLD EQUIP to recall the deployed turret.",
+        });
+      case AbilitiesIds.NANOSWARM:
+        return new BaseHability({
+          name: "Nanoswarm",
+          description:
+            "EQUIP a Nanoswarm grenade. FIRE to throw the grenade. Upon landing, the Nanoswarm goes covert. ACTIVATE the Nanoswarm to deploy a damaging swarm of nanobots.",
+        });
+      case AbilitiesIds.LOCKDOWN:
+        return new CharacterUltimateAbility({
+          name: "Lockdown",
+          description:
+            "EQUIP the Lockdown device. FIRE to deploy the device. After a long windup, the device Detains all enemies caught in the radius. The device can be destroyed by enemies.",
+        });
     }
   }
 }
 
 export enum AbilitiesIds {
+  // -------- SAGE ------
   BARRIER_ORB = 0,
   HEALING_ORB = 1,
   SLOW_ORB = 2,
   RESURRECTION = 3,
+  // -------- BRIMSTONE ------
   INCENDIARY = 4,
   SKY_SMOKE = 5,
   STIM_BEACON = 6,
   ORBITAL_STRIKE = 7,
+  // -------- PHOENIX ------
   CURVE_BALL = 8,
   HOT_HANDS = 9,
   BLAZE = 10,
   RUN_IT_BACK = 11,
+  // -------- SOVA ------
   SHOCK_BOLT = 12,
   RECON_BOLT = 13,
   OWL_DRONE = 14,
   HUNTERS_FURY = 15,
+  // -------- VIPER ------
   POISON_CLOUD = 16,
   TOXIC_SCREEN = 17,
   SNAKE_BITE = 18,
+  VIPERS_PIT = 19,
+  // -------- CIPHER ------
+  CYBER_CAGE = 20,
+  SPY_CAM = 21,
+  TRAPWIRE = 22,
+  NEUTRAL_THEFT = 23,
+  // -------- REYNA ------
+  DEVOUR = 24,
+  DISMISS = 25,
+  LEER = 26,
+  EMPRESS = 27,
+  // -------- KILLJOY ------
+  ALARMBOT = 28,
+  TURRET = 29,
+  NANOSWARM = 30,
+  LOCKDOWN = 31,
+  // -------- Breach ------
+  FLASHPOINT = 32,
 }
